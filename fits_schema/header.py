03c6eeff230f09fdabdb876a0c7af9aaefa86b0c
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2020-03-27 16:48:09 +0100

    Add more reserved header keywords

--- fits_schema/header.py
+++ fits_schema/header.py
@@ -179,30 +179,41 @@ class HeaderSchema(metaclass=HeaderSchemaMeta):
     ...        HDUCLASS = HeaderCard(required=True, allowed_values="Events")
     '''
     # define some common header keywords
+    EXTEND   = HeaderCard(type_=bool, required=False)
+    ORIGIN   = HeaderCard(type_=str, required=False)
+    TELESCOP = HeaderCard(type_=str, required=False)
+    INSTRUME = HeaderCard(type_=str, required=False)
+    OBSERVER = HeaderCard(type_=str, required=False)
+    OBJECT   = HeaderCard(type_=str, required=False)
+    AUTHOR   = HeaderCard(type_=str, required=False)
+    REFERENC = HeaderCard(type_=str, required=False)
+    COMMENT  = HeaderCard(type_=str, required=False)
+    HISTORY  = HeaderCard(type_=str, required=False)
+
     # see table 22 of https://fits.gsfc.nasa.gov/standard40/fits_standard40aa-le.pdf
-    DATE = HeaderCard(type_=str, required=False)
+    DATE     = HeaderCard(type_=str, required=False)
     DATE_OBS = HeaderCard(keyword='DATE-OBS', type_=str, required=False)
     DATE_BEG = HeaderCard(keyword='DATE-BEG', type_=str, required=False)
     DATE_AVG = HeaderCard(keyword='DATE-AVG', type_=str, required=False)
     DATE_END = HeaderCard(keyword='DATE-END', type_=str, required=False)
 
-    MJD_OBS = HeaderCard(keyword='MJD-OBS', type_=float, required=False)
-    MJD_BEG = HeaderCard(keyword='MJD-BEG', type_=float, required=False)
-    MJD_AVG = HeaderCard(keyword='MJD-AVG', type_=float, required=False)
-    MJD_END = HeaderCard(keyword='MJD-END', type_=float, required=False)
+    MJD_OBS  = HeaderCard(keyword='MJD-OBS', type_=float, required=False)
+    MJD_BEG  = HeaderCard(keyword='MJD-BEG', type_=float, required=False)
+    MJD_AVG  = HeaderCard(keyword='MJD-AVG', type_=float, required=False)
+    MJD_END  = HeaderCard(keyword='MJD-END', type_=float, required=False)
 
     # time definition
-    MJDREF  = HeaderCard(type_=float, required=False)
-    MJDREFI = HeaderCard(type_=int, required=False)
-    MJDREFF = HeaderCard(type_=float, required=False)
-    JDREF   = HeaderCard(type_=float, required=False)
-    JDREFI  = HeaderCard(type_=int, required=False)
-    JDREFF  = HeaderCard(type_=float, required=False)
-    DATEREF = HeaderCard(type_=float, required=False)
+    MJDREF   = HeaderCard(type_=float, required=False)
+    MJDREFI  = HeaderCard(type_=int, required=False)
+    MJDREFF  = HeaderCard(type_=float, required=False)
+    JDREF    = HeaderCard(type_=float, required=False)
+    JDREFI   = HeaderCard(type_=int, required=False)
+    JDREFF   = HeaderCard(type_=float, required=False)
+    DATEREF  = HeaderCard(type_=float, required=False)
 
     # start / end time relative to reference time
-    TSTART  = HeaderCard(type_=float, required=False)
-    TSTOP   = HeaderCard(type_=float, required=False)
+    TSTART   = HeaderCard(type_=float, required=False)
+    TSTOP    = HeaderCard(type_=float, required=False)
 
     @classmethod
     def validate_header(cls, header: fits.Header, onerror='raise'):
